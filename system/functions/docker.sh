62ff65a7bf2d3e682b89a08dec59464ae66f661f
Joseph Platta <joseph.platta@procore.com>
2019-05-09 08:59:15 -0700

    add keybindings for upper case and lower case

--- system/functions/docker.sh
+++ system/functions/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-start_pit_db(){
+start-pit-db(){
   docker run --rm --name pit_db -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -d -p 5433:5432 -v $HOME/docker/volumes/procore_reporting_db:/var/lib/postgresql/data postgres:10.5
 }
 
-start_prc_db(){
+start-prc-db(){
   docker run --rm --name prc_db -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=procore_db -d -p 5432:5432 -v $HOME/docker/volumes/procore_db:/var/lib/postgresql/data postgres:9.6.9
 }
